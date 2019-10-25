26a107cdafe256ae852fffd29b39f77c3576cd71
venkat010184 <muttireddy.venkat@gmail.com>
2019-10-25 20:16:40 +0530

    Commit employee pojo to staging

---
+++ Demo/src/main/java/com/example/demo/Employee.java
@@ -0,0 +1,32 @@
+package com.example.demo;
+
+public class Employee {
+
+	private int empId;
+	private String empName;
+	private float salary;
+	public int getEmpId() {
+		return empId;
+	}
+	public void setEmpId(int empId) {
+		this.empId = empId;
+	}
+	public String getEmpName() {
+		return empName;
+	}
+	public void setEmpName(String empName) {
+		this.empName = empName;
+	}
+	public float getSalary() {
+		return salary;
+	}
+	public void setSalary(float salary) {
+		this.salary = salary;
+	}
+	@Override
+	public String toString() {
+		return "Employee [empId=" + empId + ", empName=" + empName + ", salary=" + salary + "]";
+	}
+	
+	
+}
